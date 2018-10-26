006a34b0d66647734d05b4da4895f0eba8bc0744
Yana Krukovska <jana.krua@gmail.com>
2018-10-26 22:40:54 +0300

    Lab 1 task pyramid. World size changes

--- Lab1/src/Task1_pyramid.java
+++ Lab1/src/Task1_pyramid.java
@@ -7,26 +7,28 @@ public class Task1_pyramid extends GraphicsProgram {
     private static final int BRICK_WIDTH = 30;
     private static final int BRICK_HEIGHT = 12;
     private static final int BRICKS_IN_BASE = 14;
+    private static final int WORLD_WIDTH = 300;
+    private static final int WORLD_HEIGHT = 200;
 
     public void run() {
-
+        this.setSize(WORLD_WIDTH, WORLD_HEIGHT);
         for (int i = 0; i < BRICKS_IN_BASE; i++) {
             for (int j = 0; j < BRICKS_IN_BASE - i; j++) {
                 int x = calculateX(i, j);
                 int y = calculateY(i);
-                GRect brick = new GRect(x + 200, y, BRICK_WIDTH, BRICK_HEIGHT);
+                GRect brick = new GRect(x, y, BRICK_WIDTH, BRICK_HEIGHT);
                 add(brick);
             }
         }
     }
 
     private int calculateY(int i) {
-        return getHeight() - BRICK_HEIGHT * (i + 1);
+        return WORLD_HEIGHT - BRICK_HEIGHT * (i + 1);
     }
 
     private int calculateX(int i, int j) {
 
-        return (getWidth() / 2) - (BRICK_WIDTH * (BRICKS_IN_BASE - i) / 2) + j * BRICK_WIDTH;
+        return (WORLD_WIDTH / 2) - (BRICK_WIDTH * (BRICKS_IN_BASE - i) / 2) + j * BRICK_WIDTH;
 
     }
 
