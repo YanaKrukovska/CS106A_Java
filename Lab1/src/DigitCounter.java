82d63324406ef03d16707a43571bfb172f0cf5b6
Yana Krukovska <jana.krua@gmail.com>
2018-10-31 23:22:39 +0200

    Digit counter. Initial implementation

---
+++ Lab1/src/DigitCounter.java
@@ -0,0 +1,14 @@
+public class DigitCounter {
+
+    public static void main(String[] args) {
+        System.out.println(countDigits(777));
+    }
+
+    private static int countDigits(int number) {
+        if (number < 10) {
+            return 1;
+        }
+
+        return 1 + countDigits(number / 10);
+    }
+}
